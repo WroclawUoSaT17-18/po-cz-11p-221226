6599746f26b168b6994d3ea899a8e9a0d9325bf9
221226 <36729970+221226@users.noreply.github.com>
2018-06-06 23:28:20 +0200

    Add files via upload

---
+++ pejnt2/graphics/Form1.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace graphics
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton3 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton4 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton5 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton6 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton7 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripTextBox1 = new System.Windows.Forms.ToolStripTextBox();
+            this.toolStripComboBox1 = new System.Windows.Forms.ToolStripComboBox();
+            this.paleta_kolorow = new System.Windows.Forms.PictureBox();
+            this.wybrany_kolor = new System.Windows.Forms.PictureBox();
+            this.PB = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.open = new System.Windows.Forms.Button();
+            this.save = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.paleta_kolorow)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.wybrany_kolor)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PB)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1,
+            this.toolStripButton2,
+            this.toolStripButton3,
+            this.toolStripButton4,
+            this.toolStripButton5,
+            this.toolStripButton6,
+            this.toolStripButton7,
+            this.toolStripTextBox1,
+            this.toolStripComboBox1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.ShowItemToolTips = false;
+            this.toolStrip1.Size = new System.Drawing.Size(1207, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton1.Text = "toolStripButton1";
+            this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click_1);
+            // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton2.Image")));
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton2.Text = "toolStripButton2";
+            this.toolStripButton2.Click += new System.EventHandler(this.toolStripButton2_Click);
+            // 
+            // toolStripButton3
+            // 
+            this.toolStripButton3.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton3.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton3.Image")));
+            this.toolStripButton3.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton3.Name = "toolStripButton3";
+            this.toolStripButton3.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton3.Tag = "L";
+            this.toolStripButton3.Text = "LINIA";
+            this.toolStripButton3.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.toolStripButton3.Click += new System.EventHandler(this.toolStripButton3_Click);
+            // 
+            // toolStripButton4
+            // 
+            this.toolStripButton4.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton4.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton4.Image")));
+            this.toolStripButton4.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton4.Name = "toolStripButton4";
+            this.toolStripButton4.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton4.Text = "toolStripButton4";
+            this.toolStripButton4.Click += new System.EventHandler(this.toolStripButton4_Click);
+            // 
+            // toolStripButton5
+            // 
+            this.toolStripButton5.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton5.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton5.Image")));
+            this.toolStripButton5.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton5.Name = "toolStripButton5";
+            this.toolStripButton5.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton5.Text = "toolStripButton5";
+            this.toolStripButton5.Click += new System.EventHandler(this.toolStripButton5_Click);
+            // 
+            // toolStripButton6
+            // 
+            this.toolStripButton6.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton6.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton6.Image")));
+            this.toolStripButton6.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton6.Name = "toolStripButton6";
+            this.toolStripButton6.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton6.Text = "toolStripButton6";
+            this.toolStripButton6.Click += new System.EventHandler(this.toolStripButton6_Click);
+            // 
+            // toolStripButton7
+            // 
+            this.toolStripButton7.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton7.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton7.Image")));
+            this.toolStripButton7.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton7.Name = "toolStripButton7";
+            this.toolStripButton7.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton7.Text = "toolStripButton7";
+            this.toolStripButton7.Click += new System.EventHandler(this.toolStripButton7_Click);
+            // 
+            // toolStripTextBox1
+            // 
+            this.toolStripTextBox1.Name = "toolStripTextBox1";
+            this.toolStripTextBox1.Size = new System.Drawing.Size(100, 25);
+            // 
+            // toolStripComboBox1
+            // 
+            this.toolStripComboBox1.Name = "toolStripComboBox1";
+            this.toolStripComboBox1.Size = new System.Drawing.Size(121, 25);
+            // 
+            // paleta_kolorow
+            // 
+            this.paleta_kolorow.Image = ((System.Drawing.Image)(resources.GetObject("paleta_kolorow.Image")));
+            this.paleta_kolorow.Location = new System.Drawing.Point(402, 2);
+            this.paleta_kolorow.Name = "paleta_kolorow";
+            this.paleta_kolorow.Size = new System.Drawing.Size(793, 23);
+            this.paleta_kolorow.TabIndex = 1;
+            this.paleta_kolorow.TabStop = false;
+            this.paleta_kolorow.MouseDown += new System.Windows.Forms.MouseEventHandler(this.paleta_Down);
+            this.paleta_kolorow.MouseMove += new System.Windows.Forms.MouseEventHandler(this.paleta_Move);
+            this.paleta_kolorow.MouseUp += new System.Windows.Forms.MouseEventHandler(this.paleta_Up);
+            // 
+            // wybrany_kolor
+            // 
+            this.wybrany_kolor.Location = new System.Drawing.Point(0, 28);
+            this.wybrany_kolor.Name = "wybrany_kolor";
+            this.wybrany_kolor.Size = new System.Drawing.Size(44, 21);
+            this.wybrany_kolor.TabIndex = 2;
+            this.wybrany_kolor.TabStop = false;
+            // 
+            // PB
+            // 
+            this.PB.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.PB.Location = new System.Drawing.Point(0, 55);
+            this.PB.Name = "PB";
+            this.PB.Size = new System.Drawing.Size(1200, 600);
+            this.PB.TabIndex = 3;
+            this.PB.TabStop = false;
+            this.PB.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.PB.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Mouse_Click);
+            this.PB.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Mouse_Down);
+            this.PB.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Mouse_Move);
+            this.PB.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Mouse_Up);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(272, 29);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(928, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // open
+            // 
+            this.open.Location = new System.Drawing.Point(98, 26);
+            this.open.Name = "open";
+            this.open.Size = new System.Drawing.Size(75, 23);
+            this.open.TabIndex = 5;
+            this.open.Text = "open";
+            this.open.UseVisualStyleBackColor = true;
+            this.open.Click += new System.EventHandler(this.open_Click);
+            // 
+            // save
+            // 
+            this.save.Location = new System.Drawing.Point(179, 25);
+            this.save.Name = "save";
+            this.save.Size = new System.Drawing.Size(75, 23);
+            this.save.TabIndex = 6;
+            this.save.Text = "save";
+            this.save.UseVisualStyleBackColor = true;
+            this.save.Click += new System.EventHandler(this.save_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1207, 469);
+            this.Controls.Add(this.save);
+            this.Controls.Add(this.open);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.PB);
+            this.Controls.Add(this.wybrany_kolor);
+            this.Controls.Add(this.paleta_kolorow);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.paleta_kolorow)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.wybrany_kolor)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PB)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
+        private System.Windows.Forms.ToolStripButton toolStripButton3;
+        private System.Windows.Forms.ToolStripButton toolStripButton4;
+        private System.Windows.Forms.ToolStripButton toolStripButton5;
+        private System.Windows.Forms.ToolStripTextBox toolStripTextBox1;
+        private System.Windows.Forms.ToolStripComboBox toolStripComboBox1;
+        private System.Windows.Forms.PictureBox paleta_kolorow;
+        private System.Windows.Forms.PictureBox wybrany_kolor;
+        private System.Windows.Forms.ToolStripButton toolStripButton6;
+        private System.Windows.Forms.PictureBox PB;
+        private System.Windows.Forms.ToolStripButton toolStripButton7;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button open;
+        private System.Windows.Forms.Button save;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+
+    }
+}
+
---
+++ pejnt2/graphics/Form1.cs
@@ -0,0 +1,314 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace graphics
+{
+    public partial class Form1 : Form
+    {
+        Image File;
+            
+            bool wybieranie = false;
+            bool rysuj = false;
+            Color maluj_kolor;
+         int x, y, lx, ly=0;
+
+         
+            Bitmap copy; // tu będzie zapisana kopia
+           
+           
+
+            Item wybranyItem; // wybrana funkcja z listy Item
+
+        public Form1()
+        {
+         InitializeComponent();
+         Bitmap imgbmp = new Bitmap(PB.Width, PB.Height);
+         Graphics graphics = Graphics.FromImage(imgbmp);
+    
+
+         PB.Image = imgbmp;
+
+
+        }
+        public enum Item
+        { prostokat, elipsa, linia, tekst, pedzel,  gumka, pipeta}
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+          
+            copy = new Bitmap(PB.Width, PB.Height);
+
+            FontFamily[] czcionki = FontFamily.Families; //wybór czcionek
+            foreach (FontFamily font in czcionki)
+            {
+                toolStripComboBox1.Items.Add(font.GetName(1).ToString());
+            }
+        }
+
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+          
+        }
+
+
+        private void paleta(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void paleta_Down(object sender, MouseEventArgs e)
+        {
+            wybieranie = true;
+
+        }
+
+        private void paleta_Move(object sender, MouseEventArgs e)
+        {
+            if (wybieranie == true)
+            {//zbiera kolor z palety
+                Bitmap bmp = (Bitmap)paleta_kolorow.Image.Clone();
+
+                if (e.X > 0 & e.Y > 0 & e.X < paleta_kolorow.Width & e.Y < paleta_kolorow.Height)
+                {
+                    maluj_kolor = bmp.GetPixel(e.X, e.Y);
+                }
+                else { wybieranie = false; }
+
+                wybrany_kolor.BackColor = maluj_kolor;
+            }
+        }
+
+        private void paleta_Up(object sender, MouseEventArgs e)
+        {
+            wybieranie = false;
+        }
+
+        private void toolStripButton1_Click_1(object sender, EventArgs e)
+        {
+            wybranyItem = Item.prostokat;
+        }
+
+        private void toolStripButton2_Click(object sender, EventArgs e)
+        {
+            wybranyItem = Item.elipsa;
+        }
+
+        private void toolStripButton3_Click(object sender, EventArgs e)
+        {
+            wybranyItem = Item.linia;
+        }
+
+        private void toolStripButton4_Click(object sender, EventArgs e)
+        {
+            wybranyItem = Item.pedzel;
+        }
+
+        private void toolStripButton5_Click(object sender, EventArgs e)
+        {
+            wybranyItem = Item.gumka;
+        }
+
+        private void toolStripButton6_Click(object sender, EventArgs e)
+        {
+            wybranyItem = Item.tekst;
+        }
+
+
+
+        /*
+         Na Mouse_Down                     tworzy się kopia istniejącego obrazu (Bitmap copy)
+        
+         Na Mouse_Move                     przerysowuje się: imgbmp = copy + narysowana figura 
+                                           
+                                           oraz PB.Image (przestrzeń robocza programu) = imgbmp
+                             
+          */
+        private void Mouse_Up(object sender, MouseEventArgs e)
+        {
+            rysuj = false;
+            lx = e.X; // położenie kursora przy puszczeniu
+            ly = e.Y;
+       
+           
+
+          
+              
+            
+        }
+
+        private void Mouse_Move(object sender, MouseEventArgs e)
+        {
+            Bitmap imgbmp = copy.Clone() as Bitmap;
+            if (rysuj == true)//myszka kliknięta
+            {
+                
+                
+               switch (wybranyItem)
+                { 
+                    
+                    case Item.prostokat:
+                   {
+                       Graphics p = Graphics.FromImage(imgbmp);
+                       PB.Image = imgbmp;
+                     p.DrawLine(new Pen(new SolidBrush(maluj_kolor)), new Point(x, y), new Point(x, e.Y));
+                     p.DrawLine(new Pen(new SolidBrush(maluj_kolor)), new Point(x, y), new Point(e.X, y));
+                      p.DrawLine(new Pen(new SolidBrush(maluj_kolor)), new Point(e.X, e.Y), new Point(x, e.Y));
+                      p.DrawLine(new Pen(new SolidBrush(maluj_kolor)), new Point(e.X, e.Y), new Point(e.X, y));      
+                     p.Dispose();     
+                  }
+                    break;
+
+                    case Item.elipsa:
+
+                    Graphics el = Graphics.FromImage(imgbmp);
+                    PB.Image = imgbmp;
+                el.DrawEllipse(new Pen(new SolidBrush(maluj_kolor)), x, y, e.X - x, e.Y - y);
+                el.Dispose();
+                        
+
+
+                         break;
+
+                 case Item.pedzel:
+                         if (toolStripTextBox1.Text != String.Empty)
+                         {
+                        Graphics   pd= Graphics.FromImage(PB.Image);
+
+                        
+                             pd.FillEllipse(new SolidBrush(maluj_kolor), e.X - x + x, e.Y - y + y, Convert.ToInt32(toolStripTextBox1.Text), Convert.ToInt32(toolStripTextBox1.Text));
+                            copy= PB.Image.Clone() as Bitmap ;
+                           
+                             pd.Dispose();
+
+                         Graphics   g = PB.CreateGraphics(); 
+                             g.FillEllipse(new SolidBrush(maluj_kolor), e.X - x + x, e.Y - y + y, Convert.ToInt32(toolStripTextBox1.Text), Convert.ToInt32(toolStripTextBox1.Text));
+                           
+                             g.Dispose();
+                   
+
+                         }
+                           break;
+
+                    case Item.gumka: 
+                           if (toolStripTextBox1.Text != String.Empty)
+                           {
+                               Graphics gm = Graphics.FromImage(PB.Image);
+
+                               gm.FillRectangle(new SolidBrush(Color.White), e.X - x + x, e.Y - y + y, Convert.ToInt32(toolStripTextBox1.Text), Convert.ToInt32(toolStripTextBox1.Text));
+                               copy = PB.Image.Clone() as Bitmap;
+
+                               gm.Dispose();
+
+                               Graphics g = PB.CreateGraphics();
+                               g.FillRectangle(new SolidBrush(Color.White), e.X - x + x, e.Y - y + y, Convert.ToInt32(toolStripTextBox1.Text), Convert.ToInt32(toolStripTextBox1.Text));
+                         
+                              g.Dispose();
+                           }
+                           else
+                           {
+                               Graphics gm = Graphics.FromImage(PB.Image);
+
+                               gm.FillRectangle(new SolidBrush(Color.White), e.X - x + x, e.Y - y + y,30,30);
+                               copy = PB.Image.Clone() as Bitmap;
+                               gm.Dispose();
+
+                               Graphics g = PB.CreateGraphics();
+                               g.FillRectangle(new SolidBrush(Color.White), e.X - x + x, e.Y - y + y,30,30);
+                               g.Dispose();
+                           }
+                           break;
+                    case Item.linia:
+                           Graphics ln = Graphics.FromImage(imgbmp);
+
+                  ln.DrawLine(new Pen(new SolidBrush(maluj_kolor)), new Point(x, y), new Point(e.X, e.Y));
+                      ln.Dispose();
+                      PB.Image = imgbmp;
+                break;
+                }
+                
+            }
+        }
+
+        private void Mouse_Down(object sender, MouseEventArgs e)
+        {
+            rysuj = true;
+            x = e.X; //położenie kursora przy klikaniu
+            y = e.Y;
+            copy = PB.Image.Clone() as Bitmap;
+           
+           
+            
+        }
+
+        private void toolStripButton7_Click(object sender, EventArgs e)
+        {
+            wybranyItem = Item.pipeta;
+        }
+
+        private void Mouse_Click(object sender, MouseEventArgs e)
+        {
+           ;
+            
+            if (wybranyItem == Item.pipeta) //nie da się pobrać koloru bezpośrednio, dlatego trzeba zrobić screena
+            {                               // robi zrzut ekranu (prostokątna bitmapa będąca kopią przestrzeni rysowania)
+                Bitmap bmp = new Bitmap(PB.Width, PB.Height);
+                Graphics g = Graphics.FromImage(bmp);
+                Rectangle rect = PB.RectangleToScreen(PB.ClientRectangle); 
+                g.CopyFromScreen(rect.Location, Point.Empty, PB.Size);
+                g.Dispose();
+
+                maluj_kolor = bmp.GetPixel(e.X, e.Y);
+                wybrany_kolor.BackColor = maluj_kolor;
+            }
+            if (wybranyItem == Item.tekst)
+            {
+                Bitmap imgbmp = copy.Clone() as Bitmap;
+
+                Graphics tk = Graphics.FromImage(imgbmp);
+
+                int xx = e.X;
+                int yy = e.Y;
+
+
+                tk.DrawString(textBox1.Text, new Font(toolStripComboBox1.Text, Convert.ToInt32(toolStripTextBox1.Text), FontStyle.Regular), new SolidBrush(maluj_kolor), new PointF(e.X - Convert.ToInt32(toolStripTextBox1.Text), e.Y - Convert.ToInt32(toolStripTextBox1.Text)));
+              tk.Dispose();
+              PB.Image = imgbmp;
+        
+            }
+        }
+
+        private void open_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog f = new OpenFileDialog();
+            f.Filter = "JPG(*.JPG)|*.jpg";
+            if (f.ShowDialog() == DialogResult.OK)
+            {
+                File = Image.FromFile(f.FileName);
+                PB.Image = File;
+
+            }
+        }
+
+        private void save_Click(object sender, EventArgs e)
+        {
+           
+            
+            Bitmap bmp = new Bitmap(PB.Width, PB.Height );
+            bmp = PB.Image.Clone() as Bitmap;
+              bmp.Save("C:\\Users\\Piotr Wawrocki\\Desktop\\obrazki\\obraz.png");
+            }
+
+
+        }
+    }
